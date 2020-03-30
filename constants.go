6b4b659298c6db93909e4f9802a3b970a6c7914c
Hasham Ali <the.hasham.ali@gmail.com>
2020-03-29 20:31:49 -0700

    Remove JWT middleware as it is a bit too app specific.

--- constants.go
+++
@@ -1,7 +0,0 @@
-package gauth
-
-// ContextJWTMetaKey is the key within Context that holds the current UserID.
-const ContextJWTMetaKey ContextKeyType = iota
-
-// ContextKeyType is the key type for Contexts.
-type ContextKeyType int
